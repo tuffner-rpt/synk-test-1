7dbbd5a698640322d5e19e86a987507d843f08da
tuffner-rpt <80557228+tuffner-rpt@users.noreply.github.com>
2021-03-12 21:05:00 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,7 +6,10 @@ provider "aws" {
 # Create AWS S3 Bucket with Terraform
 resource "aws_s3_bucket" "tuffnertest" {
   bucket = "tuffner-tf-test-bucket-1"
-  acl    = "public"
+  acl    = "private"
+  
+  versioning = {
+    enabled = true
 
   tags = {
     Name        = "My bucket"
