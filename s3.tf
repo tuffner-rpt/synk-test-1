687b92aadc081cd8460be6b48cdc2dd231087141
tuffner-rpt <80557228+tuffner-rpt@users.noreply.github.com>
2021-03-12 21:06:22 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -8,9 +8,6 @@ resource "aws_s3_bucket" "tuffnertest" {
   bucket = "tuffner-tf-test-bucket-1"
   acl    = "private"
   
-  versioning = {
-    enabled = true
-
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
